c49c4f6f22b5df5d28de382798d65699b75ecdc1
Thomas Sanfourche <58660377+ThomasSanf@users.noreply.github.com>
2022-02-19 10:56:43 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  reactStrictMode: true,
-    eslint: {
+  eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
