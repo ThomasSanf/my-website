c17c56079f0fde7ee22f45e26797e0ad7fd39798
thomassanf <jyxfov-8dewcu-cyGdoj>
2022-02-23 09:55:12 +0100

    various small fixes

--- pages/projects.js
+++ pages/projects.js
@@ -41,7 +41,7 @@ export default function About() {
       <Hero title={"my projects."}/>
 
       <Main>
-        <SimpleGrid columns={2} spacing={10}>
+        <SimpleGrid minChildWidth='200' columns={2} spacing={10}>
       <ItemProject title={"Tampopo"} 
       thumbnail={'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'}
       type="Restaurant Website"
